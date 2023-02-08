4582ab2eba88c1ff235a002fe3ac596bbae2c175
Mihailo <mm2507@cam.ac.uk>
2023-02-08 20:30:13 +0000

    Refactored Code

---
+++ src/DataLoaders.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+import biotite
+import biotite.structure as struc
+import src.DataProcessing  as dp
+from progressbar import ProgressBar
+from typing import Optional
+
+def load_labels(path):
+    labels = pd.read_csv(path, names = ["chain_id", "label"])
+    labels[['id', 'chain']] = labels['chain_id'].str.split('.', 1, expand=True)
+    return labels
+
+def check_chain(chain: biotite.structure.AtomArray):
+    ns = chain[chain.atom_name == 'N']
+    cas = chain[chain.atom_name == 'CA']
+    cs = chain[chain.atom_name == 'C']
+    os = chain[chain.atom_name == 'O']
+    if not (set(chain.res_name).issubset(set(dp.STANDARD_AMINO_ACIDS))):
+        return False
+    if(np.array_equal(ns.res_id, cas.res_id) and np.array_equal(cas.res_id, cs.res_id) and np.array_equal(cs.res_id, os.res_id)):
+        return True
+    return False
+
+def load_structures_from_file_ids(path, labels, database, exclude: Optional[str] = None):
+    pbar = ProgressBar()
+    if exclude != None:
+        structure_set = set(line.strip() for line in open(path)).difference(set(line.strip().replace("_",".") for line in open(exclude)))
+    else:
+        structure_set = set(line.strip() for line in open(path))
+    structures = []
+    for id in pbar(structure_set):
+        PDBid, chain = id.split(".")
+        x = dp.DataPreProcessorForGNM(type_flexibility = "msqf")
+        if(database.__contains__(PDBid)):
+            structure = database[PDBid]["structure"]
+            protein_chain = structure[(structure.chain_id == chain) & struc.filter_amino_acids(structure)] 
+            if(check_chain(protein_chain)):
+                structures.append(x.from_loaded_structure(protein_chain, labels[(labels["id"] == PDBid) & (labels["chain"] == chain)].label.iloc[0]))
+    return structures
+
+def load_structures_from_labels_available(labels, database, exclude):
+    pbar = ProgressBar()
+
+    all_structures = []
+    for id in pbar(set(labels.chain_id).difference(set(line.strip().replace("_",".") for line in open('./data/enzyme_data/missing_resis.txt')))):
+        PDBid, chain = id.split(".")
+        x = dp.DataPreProcessorForGNM(type_flexibility = "msqf")
+        if(database.__contains__(PDBid)):
+            structure = database[PDBid]["structure"]
+            protein_chain = structure[(structure.chain_id == chain) & struc.filter_amino_acids(structure)] 
+            if(check_chain(protein_chain)):
+                all_structures.append(x.from_loaded_structure(protein_chain, labels[labels.chain_id == id].label.iloc[0]))
--- src/springcraftNMA.py
+++ src/springcraftNMA.py
@@ -1,4 +1,5 @@
 # This file is used to calculate/extract and plot normal modes from a file or from PDBID
+import biotite
 import biotite.structure as struc
 import biotite.structure.io.mmtf as mmtf
 import biotite.database.rcsb as rcsb
@@ -6,6 +7,9 @@ import biotite.structure.io as strucio
 import springcraft
 import scipy
 import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from typing import Literal
 
 def calculate_normal_modes_gnm_from_file(path, cutoff):
     '''
@@ -15,8 +19,7 @@ def calculate_normal_modes_gnm_from_file(path, cutoff):
         path : string - Path to .pdb file containing structure we want to explore
         cutoff : double - Angstrom distance under which two CA atoms are represented as connected
     Returns:
-        eigenvalues : array(double) - Eigenvalues of Kirchhoff matrix
-        eigenvectors : matrix(double) - Eigenvectors of Kirchhoff matrix represented as rows of the matrix returned   
+        gnm : springcraft.GNM - class that contains Kirchhoff matrix and can call calculation of normal modes
     '''
 
     # Read .pdb file and filter CA atoms
@@ -39,8 +42,7 @@ def calculate_normal_modes_gnm_from_id(id, cutoff):
         id : string - PDBID of a structure we want to explore
         cutoff : double - Angstrom distance under which two CA atoms are represented as connected
     Returns:
-        eigenvalues : array(double) - Eigenvalues of Kirchhoff matrix
-        eigenvectors : matrix(double) - Eigenvectors of Kirchhoff matrix represented as rows of the matrix returned   
+        gnm : springcraft.GNM - class that contains Kirchhoff matrix and can call calculation of normal modes
     '''
 
     # Read .pdb file and filter CA atoms
@@ -64,8 +66,7 @@ def calculate_normal_modes_anm_from_file(path, cutoff):
         path : string - Path to .pdb file containing structure we want to explore
         cutoff : double - Angstrom distance under which two CA atoms are represented as connected
     Returns:
-        eigenvalues : array(double) - Eigenvalues of Kirchhoff matrix
-        eigenvectors : matrix(double) - Eigenvectors of Kirchhoff matrix represented as rows of the matrix returned   
+        anm : springcraft.GNM - class that contains Kirchhoff matrix and can call calculation of normal modes
     '''
 
     # Read .pdb file and filter CA atoms
@@ -88,8 +89,7 @@ def calculate_normal_modes_anm_from_id(id, cutoff):
         id : string - PDBID of a structure we want to explore
         cutoff : double - Angstrom distance under which two CA atoms are represented as connected
     Returns:
-        eigenvalues : array(double) - Eigenvalues of Kirchhoff matrix
-        eigenvectors : matrix(double) - Eigenvectors of Kirchhoff matrix represented as rows of the matrix returned   
+        anm : springcraft.GNM - class that contains Kirchhoff matrix and can call calculation of normal modes
     '''
 
     # Read .pdb file and filter CA atoms
@@ -124,3 +124,85 @@ def pseudo_fluctuation_measure(eval, evec, K = 10, funct = (lambda x: 1/x)):
     evec = evec.transpose()
     pseudo_fluc = (w*evec).sum(axis=1)
     return torch.from_numpy(pseudo_fluc)
+
+def plot_flexibility_value(id, cutoff, k = 10, flex: Literal["msqf", "bfact", "pseudo"] = "msqf"):
+    '''
+    Function that plots flexibility value against all residues.
+
+    Arguments:
+        id : string - PDB ID of the structure
+        cutoff : double - cutoff distance for NMA analysis
+        k : int - number of modes to be considered
+        flex : Literal - type of flexibility value to plot  
+    '''
+    gnm = calculate_normal_modes_gnm_from_id(id, cutoff)
+    if flex == "msqf":
+        msqf = gnm.mean_square_fluctuation(mode_subset = np.array([i for i in range(1, k)]))
+        fig = plt.figure(figsize=(8.0, 4.0), constrained_layout=True)
+        grid = fig.add_gridspec(nrows=1, ncols=2)
+        ax = fig.add_subplot(grid[0, :])
+
+        biotite_c = biotite.colors["orange"]
+
+        ax.bar(x=np.arange(1, len(msqf)+1), height=msqf, color=biotite_c)
+        ax.set_xlabel("Amino Acid Residue ID", size=16)
+        ax.set_ylabel("Mean squared fluctuation / A.U.", size=16)
+
+        plt.show()
+    elif flex == "bfact":
+        bfact = gnm.bfactor(mode_subset = np.array([i for i in range(1, k)]))
+        fig = plt.figure(figsize=(8.0, 4.0), constrained_layout=True)
+        grid = fig.add_gridspec(nrows=1, ncols=2)
+        ax = fig.add_subplot(grid[0, :])
+
+        biotite_c = biotite.colors["orange"]
+
+        ax.bar(x=np.arange(1, len(bfact)+1), height=bfact, color=biotite_c)
+        ax.set_xlabel("Amino Acid Residue ID", size=16)
+        ax.set_ylabel("B Factor", size=16)
+    elif flex == "pseudo":
+        eigenval, eigenvec = gnm.eigen() 
+        pseudo_fluc = pseudo_fluctuation_measure(eigenval, eigenvec, k, lambda x: 1/x)
+        fig = plt.figure(figsize=(8.0, 4.0), constrained_layout=True)
+        grid = fig.add_gridspec(nrows=1, ncols=2)
+        ax = fig.add_subplot(grid[0, :])
+
+        biotite_c = biotite.colors["orange"]
+
+        ax.bar(x=np.arange(1, len(pseudo_fluc)+1), height=pseudo_fluc, color=biotite_c)
+        ax.set_xlabel("Amino Acid Residue ID", size=16)
+        ax.set_ylabel("Pseudo Fluctuation Measure", size=16)
+    else:
+        raise ValueError("Not appropriate flex type.")
+    
+def plot_eigval_freq(id, cutoff):
+    '''
+    Function that plots eigenvalues and frequencies agains all normal modes.
+
+    Arguments:
+        id : string - PDB ID of the structure
+        cutoff : double - cutoff distance for NMA analysis
+    '''
+    gnm = calculate_normal_modes_gnm_from_id(id, cutoff)
+    eigenval, eigenvec = gnm.eigen() 
+
+    freq = gnm.frequencies()[1:]
+
+    eigenval = eigenval[1:]
+    fig = plt.figure(figsize=(8.0, 4.0), constrained_layout=True)
+    grid = fig.add_gridspec(nrows=1, ncols=2)
+
+    ax00 = fig.add_subplot(grid[0, 0])
+    ax01 = fig.add_subplot(grid[0, 1])
+
+    biotite_c = biotite.colors["orange"]
+
+    ax00.bar(x=np.arange(1, len(eigenval)+1), height=eigenval, color=biotite_c)
+    ax01.bar(x=np.arange(1, len(freq)+1), height=freq, color=biotite_c)
+
+    ax00.set_xlabel("Mode", size=16)
+    ax00.set_ylabel(r"Eigenvalue $\lambda$", size=16)
+    ax01.set_xlabel("Mode", size=16)
+    ax01.set_ylabel(r"Frequency $\nu$ / A.U.", size=16)
+
+    plt.show()
